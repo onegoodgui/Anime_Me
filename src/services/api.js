47822070d8a56d3e5b1d2e41bf1560c42e884559
Guilherme Dallagnol Vargas <gui_dvargas@hotmail.com>
2022-07-01 19:57:07 -0300

    feat: vercel deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,48 +1,47 @@
 import axios from "axios";
 
-const BASE_URL = 'https://anime-me-api.herokuapp.com'
-
+const BASE_URL = "https://anime-me-api.herokuapp.com";
+//forced push
 
 function createConfig(token) {
-    return { headers: { Authorization: `Bearer ${token}` } };
-  }
-  
-  function signUp(signUpData) {
-    return axios.post(`${BASE_URL}/sign-up`, signUpData);
-  }
-  
-  function signIn(signInData) {
-    return axios.post(`${BASE_URL}/sign-in`, signInData);
-  }
-
-function getAnimes(token, score){
-    const config = createConfig(token);
-    const result = axios.get(`${BASE_URL}/?score=${score}`, config);
-    return result;
-
+  return { headers: { Authorization: `Bearer ${token}` } };
 }
 
-function addAnime(token, status, animeData){
+function signUp(signUpData) {
+  return axios.post(`${BASE_URL}/sign-up`, signUpData);
+}
+
+function signIn(signInData) {
+  return axios.post(`${BASE_URL}/sign-in`, signInData);
+}
+
+function getAnimes(token, score) {
   const config = createConfig(token);
-  const animeObject = {status, ...animeData}
-  return axios.post(`${BASE_URL}`,animeObject, config);
+  const result = axios.get(`${BASE_URL}/?score=${score}`, config);
+  return result;
 }
 
-function getAnimeList(token){
+function addAnime(token, status, animeData) {
+  const config = createConfig(token);
+  const animeObject = { status, ...animeData };
+  return axios.post(`${BASE_URL}`, animeObject, config);
+}
+
+function getAnimeList(token) {
   const config = createConfig(token);
   return axios.get(`${BASE_URL}/animelist`, config);
 }
 
-function updateAnimeStatus(token, statusData){
+function updateAnimeStatus(token, statusData) {
   const config = createConfig(token);
-  return axios.put(`${BASE_URL}/status`,statusData,config)
+  return axios.put(`${BASE_URL}/status`, statusData, config);
 }
 
 export const api = {
-    signUp,
-    signIn,
-    getAnimes,
-    addAnime,
-    getAnimeList,
-    updateAnimeStatus
-}
+  signUp,
+  signIn,
+  getAnimes,
+  addAnime,
+  getAnimeList,
+  updateAnimeStatus,
+};
